4d90d754d46f966e7f5a4e4e8f6f060cecb6290f
Harsh Verma <72095207+Hs-14@users.noreply.github.com>
2024-04-24 18:33:22 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
 // const websiteLink = 'http://127.0.0.1:8000';
-const websiteLink = 'https://67e18272dccc0fd2b45e19349cf44cae.loophole.site';
+const websiteLink = 'https://a3bf133e9ff0ba768a6f1f7b0456d749.loophole.site';
